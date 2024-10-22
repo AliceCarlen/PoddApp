6efd22e80eb5d326cfb825c68b1af1bb250e6397
AliceCarlen <alice1998@live.se>
2024-10-22 11:08:46 +0200

    kommentarer

--- DAL/DAL.Kategorier.cs
+++ DAL/DAL.Kategorier.cs
@@ -8,29 +8,20 @@ namespace PoddApp.DAL
 {
     public class KategoriDataAccess //datalagret har här färdiga kategorier som ligger inom metoden HamtaKategorier.
     {
-        //public List<string> HamtaKategorier()
-        //{
-        //    return new List<string> { "Humor", "Hälsa", "Utbildning", "True crime", "Historia" };
-        //}
-
-        //public void LaggTillKategori(string nyKategori)
-        //{
-        //    HamtaKategorier().Add(nyKategori);
-        //}
 
         private List<string> kategorier;
 
-        public KategoriDataAccess()
+        public KategoriDataAccess() 
         {
             kategorier = new List<string> { "Humor", "Hälsa", "Utbildning", "True crime", "Historia" };
         }
 
-        public List<string> HamtaKategorier()
+        public List<string> HamtaKategorier() //returnerar en lista med kategorier
         {
             return kategorier;
         }
 
-        public void LaggTillKategori(string nyKategori)
+        public void LaggTillKategori(string nyKategori) //lägger till ny kategori i listan
         {
             if (!string.IsNullOrEmpty(nyKategori))
             {
